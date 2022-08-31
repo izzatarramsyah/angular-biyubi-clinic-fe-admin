b63132ef3ed909a0aaf21f1d153789d8f9e312fa
adl.izzata <adl.izzata@xl.co.id>
2022-08-31 14:35:57 +0700

    commit

---
+++ src/environments/environment.local.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    envName: 'local',
+    apiUrl: 'http://localhost:8888'
+  };
+  
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  envName: 'prod',
+  apiUrl: 'https://biyubi-clinic-backend-admin.herokuapp.com'
+};
