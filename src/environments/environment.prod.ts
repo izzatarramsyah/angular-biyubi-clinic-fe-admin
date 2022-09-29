d5fa5ff5eb8b95020e902fae89ce9bee820e0538
adl.izzata <adl.izzata@xl.co.id>
2022-09-29 17:45:52 +0700

    fixing endpoint prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   envName: 'prod',
-  apiUrl: 'https://biyubi-clinic-backend-admin.herokuapp.com',
+  apiUrl: 'https://biyubi-clinic-backend.herokuapp.com',
   socketUrl: 'https://biyubi-whatsapp-api.herokuapp.com'
 };
